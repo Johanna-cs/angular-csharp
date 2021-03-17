d22ccd59834a2fac22c72983cc180ab0286552ee
Johanna <j.cas@alise.net>
2021-03-17 16:24:48 +0100

    Managed counter title ok with syncfusion textbox

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LaConcessionComponent } from './la-concession/la-concession.component';
 import { CircularGaugeModule } from '@syncfusion/ej2-angular-circulargauge';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
